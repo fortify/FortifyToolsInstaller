54fbcde2c8c008c7537acfc3891c47f0f833be47
Ruud Senden <ruud.senden@microfocus.com>
2021-12-14 10:56:26 +0100

    feat: Add support for latest FortifyVulnerabilityExporter version 1.5.1

--- FortifyToolsInstaller.sh
+++ FortifyToolsInstaller.sh
@@ -70,7 +70,8 @@ defineTools() {
 	setToolDefaultVersion FortifyVulnerabilityExporter latest
 	addToolDownloadUrl    FortifyVulnerabilityExporter default 'https://github.com/fortify/FortifyVulnerabilityExporter/releases/download/${toolVersion}/FortifyVulnerabilityExporter.zip'
 	addToolDownloadUrl    FortifyVulnerabilityExporter latest  'https://github.com/fortify/FortifyVulnerabilityExporter/releases/latest/download/FortifyVulnerabilityExporter.zip'
-	addToolSHA256         FortifyVulnerabilityExporter latest  'cc86851b0b5adada0b04f43b93693a637ab3735e85d25246f859535aeb7835cf'
+	addToolSHA256         FortifyVulnerabilityExporter latest  '8db8d6db6c7d3e6ddeee0722179cb0f413a0d3dcefa19a21db5461a0b0c410d4'
+	addToolSHA256         FortifyVulnerabilityExporter v1.5.1  '8db8d6db6c7d3e6ddeee0722179cb0f413a0d3dcefa19a21db5461a0b0c410d4'
 	addToolSHA256         FortifyVulnerabilityExporter v1.5.0  'cc86851b0b5adada0b04f43b93693a637ab3735e85d25246f859535aeb7835cf'
 	addToolSHA256         FortifyVulnerabilityExporter v1.4.1  '6e9fa005364513593ab820c79d2c1f05283ff9769fb313c669f0159c395e3d4c'
 	addToolSHA256         FortifyVulnerabilityExporter v1.4.0  '63b6e90c5a06f3db6d913121a5fb8a7578d2a1c65a4bdc8cfd6fd0aef286d296'
